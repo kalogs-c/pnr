8259dfe9dbcd7f92990c687e6981bc4b183d86a8
Carlos Henrique Camilo Guimarães <cg@inicie.digital>
2024-09-11 20:07:43 -0300

    lang def

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "mpc.h"
 #include <editline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +7,18 @@
 #define FALSE 0
 
 int main() {
+  mpc_parser_t *Number = mpc_new("number");
+  mpc_parser_t *Operator = mpc_new("operator");
+  mpc_parser_t *Expr = mpc_new("expr");
+  mpc_parser_t *Lispy = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+            " number : /-?[0-9]+/ ; "
+            " operator : '+' | '-' | '*' | '/' ;                  "
+            " expr     : <number> | '(' <operator> <expr>+ ')' ;  "
+            " lispy    : /^/ <operator> <expr>+ /$/ ;             ",
+            Number, Operator, Expr, Lispy);
+
   puts("Lispy Version 0.0.1");
   puts("Press Ctrl+C to exit\n");
 
@@ -16,5 +29,7 @@ int main() {
     printf("Amo %s\n", input);
     free(input);
   }
+
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   return 0;
 }
